4744c684422a34d3e78fa524da69f4b7a73b2d7b
giovannyortegon <ortegon.giovanny@hotmail.com>
2021-06-11 12:40:30 -0500

    [UPDATE] adding comments

--- CPP-Projects/3.EstructurasDeAlmacenamientoPOO/3-semana/Task/Evidencia3.cpp
+++ CPP-Projects/3.EstructurasDeAlmacenamientoPOO/3-semana/Task/Evidencia3.cpp
@@ -1,3 +1,7 @@
+/**
+ * Evidencia 3: Calcula el salario a pagar despues de Descuentos
+ * Autor:       Giovanny Ortegon
+*/
 #include<iostream>
 #include<string>
 #include<iomanip>
@@ -8,17 +12,47 @@
 
 using namespace std;
 
-#define MAX 255
-#define BUF 30
-#define EMPLEADOS 100
-#define HORAEXTRA 6645
+/* Global Variables */
+#define MAX 255             // caracteres a extraer 
+#define BUF 30              // caracteres a escribir
+#define EMPLEADOS 100       // maximo de empleados a calcular
+#define HORAEXTRA 6645      // equvalente a una hora extra
 
+/**
+ * nombre -  nombre del empleado
+ * cargo - ocupacion del empleado dentro de la empresaa
+ */
 char nombre[EMPLEADOS][BUF], cargo[EMPLEADOS][BUF];
+
+/**
+ * cadula - numero de cedula del empleado
+ * telefono - numero telefonico
+ * salario basico - salario del empleado
+*/
 int cedula[EMPLEADOS], telefono[EMPLEADOS], salario_basico[EMPLEADOS];
+
+/**
+ * HExtraT - Horas Extra modo tiempo
+ * HExtraD - Horas Exrta modo Dinero
+ * DXPrestamo - Descuentos por prestamo
+ */
 double HExtraT[EMPLEADOS], HExtraD[EMPLEADOS], DXPrestamo[EMPLEADOS];
+
+/**
+ * Ahorro - Dinero ahorrado
+ * SegSocP - Porcentaje de descuento seguro social
+ * SegSocD - Dinero descontado de acuerdo al porcentaje por seguro social
+*/
 double Ahorro[EMPLEADOS], SegSocP[EMPLEADOS], SegSocD[EMPLEADOS];
+
+/**
+ * SalarioTotal - Dinero restante despues de descuentos mas dinero ahorrado
+*/
 double SalarioTotal[EMPLEADOS];
 
+/**
+ * Prototipos de funciones I/O y operaciones
+*/
 void titulo_formulario();
 void info_empleado();
 void input_datos_empleados(const unsigned int);
@@ -27,6 +61,10 @@ void mostar_datos_empleados(const unsigned int);
 double HorasExtra(double );
 void DescuentoPorSeguro(double );
 
+/**
+ * main - puento de entrada
+ * return: Siempre retorna 0 si finaliza exitosamente
+*/
 int main(void)
 {
     int n_empleados;
@@ -35,8 +73,14 @@ int main(void)
 	cout <<"******* Evidencia - Semana 3 *******";
 	cout <<"\n************************************\n\n";
 
+    /* Ingresa cantidad de empleados */
     cout <<"De cuantos empleados desea generar pagos? ";
     cin >> n_empleados;
+    if (n_empleados > EMPLEADOS)
+    {
+        cout <<"[!] Excede la cantidad maxima.";
+        exit(-1);
+    }
 
 	titulo_formulario();
 	input_datos_empleados(n_empleados);
@@ -51,6 +95,31 @@ int main(void)
 
 	return (0);
 }
+
+/**
+ * titulo_formulario - Muestra titulo de inicio de registro
+*/
+void titulo_formulario()
+{
+	cout <<"\n***********************************\n";
+	cout <<"**** Formulario datos Empleado ****";
+	cout <<"\n***********************************\n";
+}
+
+/**
+ * info_empleado - Imprime encabezado sobre informacion empleado
+*/
+void info_empleado()
+{
+	cout <<"\n************************************\n";
+	cout <<"***** Informacion del Empleado *****";
+	cout <<"\n************************************\n";
+}
+
+/**
+ * input_datos_empleados - Entrada de informacion del empelado
+ * n_empleado:  cantidad de empleados a calcular salario
+*/
 void input_datos_empleados(const unsigned int n_empleados)
 {
     unsigned int i = 0;
@@ -117,23 +186,22 @@ void input_datos_empleados(const unsigned int n_empleados)
         i++;
     }
 }
-void titulo_formulario()
-{
-	cout <<"\n***********************************\n";
-	cout <<"**** Formulario datos Empleado ****";
-	cout <<"\n***********************************\n";
-}
-void info_empleado()
-{
-	cout <<"\n************************************\n";
-	cout <<"***** Informacion del Empleado *****";
-	cout <<"\n************************************\n";
-}
 
+/**
+ * HorasExtra -  Calcula en Dinero horas extras
+ * cant:        cantidad de horas realizadas
+ * return:      Horas Extras en Dinero
+*/
 double HorasExtra(double cant)
 {
 	return HORAEXTRA * cant;
 }
+
+/**
+ * salario_total - Calcula el salario a pagar despues de descuentos
+ * idx:         index del empleado a calcular salario total
+ * return:      Salario a pagar
+ */
 double salario_total(const unsigned int idx)
 {
 	int salario = (salario_basico[idx] + HExtraD[idx]);
@@ -141,6 +209,12 @@ double salario_total(const unsigned int idx)
 
 	return salario - SegSocD[idx] - DXPrestamo[idx] + Ahorro[idx];
 }
+
+/**
+ * mostrar_datos_empleado - Imprime el registro total del empleado
+ *                           con los calculos realizados
+ * n_empleado:      cantidad de empleados
+*/
 void mostar_datos_empleados(const unsigned int n_empleados)
 {
 	unsigned int i = 0;
