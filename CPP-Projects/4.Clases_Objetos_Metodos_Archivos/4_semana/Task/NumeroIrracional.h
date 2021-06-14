5b0ca9f60974ea71ea8613690ebff981678a3266
giovannyortegon <ortegon.giovanny@hotmail.com>
2021-06-14 13:58:21 -0500

    [ADD/UPDATE] operation log/ new functions

--- CPP-Projects/4.Clases_Objetos_Metodos_Archivos/4_semana/Task/NumeroIrracional.h
+++ CPP-Projects/4.Clases_Objetos_Metodos_Archivos/4_semana/Task/NumeroIrracional.h
@@ -2,21 +2,31 @@
 #define _NUMEROIRRACIONAL_H_
 
 #include<iostream>
+#include<string>
+#include<iomanip>
+#include<ctime>
+#include<cstring>
+
 
 using namespace std;
 
+/* Clase para realizar operaciones con numeros imaginarios */
 class NumeroIrracional
 {
+	// Atributos
 	private:
 		double entera;
 		double imaginaria;
 
+	// Metodos
 	public:
 		NumeroIrracional();
 		NumeroIrracional(double, double);
 		~NumeroIrracional();
-		void Mostrar();
+		string Mostrar();
+		string take_time();
 
+	// funciones Friend para sobrecargar los operadores
 	friend NumeroIrracional operator + (NumeroIrracional, NumeroIrracional);
 	friend NumeroIrracional operator - (NumeroIrracional, NumeroIrracional);
 	friend NumeroIrracional operator * (NumeroIrracional, NumeroIrracional);
