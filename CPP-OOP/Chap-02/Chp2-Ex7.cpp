1991d2cb134360d0d8767f0e6a72173870eebed4
giovannyortegon <ortegon.giovanny@hotmail.com>
2021-09-24 23:10:03 -0500

    [ADD] constant and prototype function

--- CPP-OOP/Chap-02/Chp2-Ex7.cpp
+++ CPP-OOP/Chap-02/Chp2-Ex7.cpp
@@ -6,17 +6,18 @@ int maximum(double, double);
 
 int main()
 {
-    int result;
-    int m = 6, n = 10;
-    float x = 5.7, y = 9.89;
+	int result;
+	int m = 6, n = 10;
+	float x = 5, y = 9.89;
 
-    result = maximum(x, y);
-    cout << "Resutl is: " <<maximum(m, n) <<endl;
-    cout << "The maximum is: " <<maximum(m, n) <<endl;
+	result = maximum(x, y);
+	cout <<"Result is: " <<result <<endl;
+	cout <<"The maximum is: " <<maximum(m, n) <<endl;
 
-    return (0);
+	return (0);
 }
+
 int maximum(double a, double b)
 {
-    return (a > b) ? a : b;
+	return (a > b) ? a : b;
 }
